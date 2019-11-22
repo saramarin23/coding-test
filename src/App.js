416a780d4422de3eaf9e68e5070dcb6f385a9c86
saramarin23 <saramarin.econ@gmail.com>
2019-11-22 22:00:53 +0100

    add basic styles with mock data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,63 @@
 import React from "react";
+import Logo from "./assets/images/mediasmart.png";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+// import getDataFromServer from "./service/data";
+// const URL = "./service/employees.json";
+
+const mockPeople = [
+  {
+    name: "John Doe",
+    age: 28
+  },
+  {
+    name: "Linus Torvals",
+    age: 49
+  }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mockPeople
+    };
+  }
+
+  componentDidMount() {
+    fetch(mockPeople).then(response => response.json());
+    //    {
+    //   method: "GET"
+    // })
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     if (res.success) {
+    //       //mensaje correcto
+    //     } else {
+    //       //mensaje de error
+    //     }
+    //   })
+    //   .catch(function() {
+    //     alert("Can't connect to backend try latter");
+    //   });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <div className="App-header">
+          {/* <div> */}
+          <img src={Logo} alt="mediasmart-logo" className="logo" />
+          {/* </div>
+          <div> */}
+          <p>MediaSmart Members</p>
+          {/* </div> */}
+        </div>
+        <main></main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/service/data.js
@@ -0,0 +1,11 @@
+const URL = "./service/employees.json";
+
+const getDataFromServer = () => {
+  return fetch(URL).then(res => res.json);
+};
+
+export default getDataFromServer;
+
+// export const fetchURL = () => {
+//   console.log(fetch("http://work.mediasmart.io/"));
+// };
